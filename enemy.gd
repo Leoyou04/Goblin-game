6eb5166acd8022b3073a08dd45003c02b24cfad6
Leoyou04 <leoyou04@kingshigh.school.nz>
2025-06-04 12:26:33 +1200

    1.8
    I built the bat model

--- enemy.gd
+++ enemy.gd
@@ -1,36 +1,26 @@
-extends CharacterBody2D
+extends Node2D
 
+@export var speed := 200
+@onready var area = $Area2D
 
-@export var follow_speed :=100
-@export var player_path :=NodePath("")
-
-var player = null
-var should_follow := false
+var player: Node2D = null
+var following := false
 
 func _ready():
-	player = get_node_or_null(player_path)
-	$Detection_area.body_entered.connect(_on_body_entered)
-	$Detection_area.body_exited.connect(_on_body_exited)
-	
-	
-func _physics_process(delta):
-	if player == null:
-		return
-		
-	var direction = (player.global_position - global_position).normlaized()
-	velocity = direction * follow_speed
-	move_and_slide()
-	
-func _on_body_entered(body):
-	if body.name =="player":
-		should_follow = true
-		print("player detected")
-		
-func _on_body_exited(body):
-	if body.name =="player":
-		should_follow = false
-		print("player lost")
-	
+	area.connect("body_entered", _on_body_entered)
+	area.connect("body_exited", _on_body_exited)
 
-func _on_detection_area_body_entered(body):
-	pass # Replace with function body.
+func _process(delta):
+	if following and player:
+		var direction = (player.global_position - global_position).normalized()
+		position += direction * speed * delta
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		player = body
+		following = true
+
+func _on_body_exited(body):
+	if body == player:
+		following = false
+		player = null
