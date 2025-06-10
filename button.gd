bf82b1ca0aa51457671d02f369623ee60926b9f6
Leoyou04 <leoyou04@kingshigh.school.nz>
2025-06-11 10:56:40 +1200

    2.2
    fixed title screen

--- button.gd
+++ button.gd
@@ -1,6 +1,4 @@
 extends Button
 
-# Button's script
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://path/to/main.tscn")
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
